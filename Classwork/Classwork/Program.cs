dc221b545f64013e6a0f56e0ef103c1798b4806a
KRONIK666 <kronik@abv.bg>
2017-01-30 20:40:32 +0200

    Classwork

---
+++ Classwork/Classwork/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+
+            a = a - b;
+            b = a + b;
+            a = b - a;
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+        }
+    }
+}
