9c6e699252c5b9c785cdd0624672a93fc6e7e717
KRONIK666 <kronik@abv.bg>
2017-03-24 14:07:59 +0200

    Delete All

--- Demos/HRManagementSystem/HRManagementSystem/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HRManagementSystem.MvcApplication" Language="C#" %>
--- Demos/StartupBusinessSystem/Web/StartupBusinessSystem.Web/ViewModels/Account/ExternalLoginListViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace StartupBusinessSystem.Web.ViewModels.Account
-{
-    public class ExternalLoginListViewModel
-    {
-        public string ReturnUrl { get; set; }
-    }
-}
